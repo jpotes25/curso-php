cf8ead852618aec8d1c6e620eb690f4624702ffa
David Potes <jdpotesp@ut.edu.co>
2021-05-26 20:02:21 -0500

    Issue resolved

--- arrays.php
+++ arrays.php
@@ -12,7 +12,7 @@ var_dump($cursos);
  * 
  */
 
-$cursosC = [
+$cursosComplejo = [
     'frontend'=> 'javascript',
     'framework'=> 'Laravel',
     'backend'=> 'php'
@@ -24,7 +24,7 @@ $cursosC = [
  * y ademas de eso podemos asignarle  una funcion, para que en cada recorrido, cumpla algo
  * 
  */
-foreach ($cursosC as $key => $value) {
+foreach ($cursosComplejo as $key => $value) {
     echo "$key: $value <br>";
 }
 
@@ -33,22 +33,22 @@ function upper($curso)
 {
     echo strtoupper($curso) . "<br>";
 }
-array_walk($cursosC, 'upper');
+array_walk($cursosComplejo, 'upper');
 
 # Funciones interesantes con arrays
 
 # array_key_exists(str key, var array) = esta funcion busca en nuestro array una llave y nos dice si existe
 
-var_dump(array_key_exists('frontend', $cursosC));
+var_dump(array_key_exists('frontend', $cursosComplejo));
 
 # in_array(srt valor, var array) = esta funcion busca en nuestro array un valor y nos dice si existe
 
-var_dump(in_array('javascript', $cursosC));
+var_dump(in_array('javascript', $cursosComplejo));
 
 # array_keys(var array) = esta funcion muestra todas las llaves de un array
 
-var_dump(array_keys($cursosC));
+var_dump(array_keys($cursosComplejo));
 
 # array_values(var array) = esta funcion muestra todos los valores de un array
 
-var_dump(array_values($cursosC));
+var_dump(array_values($cursosComplejo);
